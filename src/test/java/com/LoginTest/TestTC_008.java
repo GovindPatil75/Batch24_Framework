b1e2ea7799ab6d2f6478f9a7a5806776e4e33b8a
abhirajrajgure <abhirajrajgure8@gmail.com>
2024-04-30 08:59:43 +0530

    test008

---
+++ src/test/java/com/LoginTest/TestTC_008.java
@@ -0,0 +1,26 @@
+package com.LoginTest;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import com.PageObject.LoginPom;
+import com.Utility.BaseClass;
+import com.Utility.Library;
+
+public class TestTC_008 extends BaseClass{
+	
+	@Test
+	public void verifyTC_004()   {
+		
+		LoginPom login=PageFactory.initElements(driver, LoginPom.class);
+		
+		String Username=excel.getStringTestData("LoginTest", 0, 0);
+		String Password=excel.getStringTestData("LoginTest", 0, 1);
+		
+		
+		Library.custom_Sendkeys(login.getEmail(), Username ,"Email");
+		Library.custom_Sendkeys(login.getPassword(), Password ,"Password");
+		Library.custom_Click(login.getBtnLogin() ,"Login Button");
+
+}
+}
